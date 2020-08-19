db68f0cf1a3a3e54a6f92133e736940f1fa440ae
Andrew Brown <andrew.brown@intel.com>
2020-08-19 15:59:08 -0700

    Improve build times
    This change makes it easier to: 1) specify which OpenVINO plugins are built using Cargo features and 2) specify a directory of pre-built OpenVINO libraries.

--- README.md
+++ README.md
@@ -28,6 +28,17 @@ If the build fails due to missing system dependencies, take a first look at this
 for a quick overview but refer to the [OpenVINO build documentation](https://github.com/openvinotoolkit/openvino/blob/master/build-instruction.md)
 for the details.
 
+OpenVINO has a plugin system for device-specific libraries (e.g. GPU); building all of these libraries along with the
+core inference libraries can take >20 minutes. To avoid over-long build times, [openvino-sys] exposes several features:
+ - by default, [openvino-sys] will only build the CPU plugin; to build all plugins, use `--features all` (see
+ [Cargo.toml](crates/openvino-sys/Cargo.toml)).
+ - optionally, you can provide pre-built libraries using the `OPENVINO_LIB_DIR` environment variable., e.g.
+ `OPENVINO_LIB_DIR=../openvino/build/bin/intel64/Release/lib cargo build`. This directory should contain all of the
+ core libraries, plugin libraries, and `plugins.xml` (see [build.rs](crates/openvino-sys/build.rs) for CMake
+ configuration). Note that if this path is outside of the `target` directory, Cargo [will expect you to set the library
+ path](https://doc.rust-lang.org/cargo/reference/environment-variables.html#dynamic-library-paths) (e.g. 
+ `LD_LIBRARY_PATH`) on your own.
+
 
 
 ### Use
--- crates/openvino-sys/build.rs
+++ crates/openvino-sys/build.rs
@@ -1,6 +1,7 @@
+use std::path::{Path, PathBuf};
+
 use bindgen;
 use cmake;
-use std::path::{Path, PathBuf};
 
 fn main() {
     // Trigger rebuild on changes to build.rs and Cargo.toml and every source file.
@@ -30,23 +31,50 @@ fn main() {
         .expect("failed to write bindings.rs");
 
     // Build OpenVINO using CMake.
-    let build_path = cmake::Config::new("../upstream")
-        .define("NGRAPH_ONNX_IMPORT_ENABLE", "ON")
-        .define("ENABLE_OPENCV", "OFF")
-        .very_verbose(true)
-        .build();
+    let library_path = if let Some(path) = std::env::var_os("OPENVINO_LIB_DIR") {
+        let library_path = dir(path);
+        assert!(
+            library_path.join("plugins.xml").is_file(),
+            "The OPENVINO_LIB_DIR should contain a plugins.xml file"
+        );
+        library_path
+    } else {
+        fn cmake(out_dir: &str) -> cmake::Config {
+            let mut config = cmake::Config::new("../upstream");
+            config
+                .very_verbose(true)
+                .define("NGRAPH_ONNX_IMPORT_ENABLE", "ON")
+                .define("ENABLE_OPENCV", "OFF")
+                .define("ENABLE_CPPLINT", "OFF")
+                // Because OpenVINO by default wants to build its binaries in its own tree, we must specify
+                // that we actually want them in Cargo's output directory.
+                .define("OUTPUT_ROOT", out_dir);
+            config
+        }
 
-    // Set up the link search path to the manually-built library. It would be preferable to use
-    // pkg-config here to retrieve the libraries if they were installed system-wide (see initial
-    // commit), but OpenVINO relies on a `plugin.xml` file for finding target-specific libraries
-    // and it is unclear how pkg-config where this would be in the system-install scenario.
-    let library_path = build_path.join("deployment_tools/inference_engine/lib/intel64");
+        // Specifying the build targets reduces the build time somewhat; this one will trigger
+        // builds for other necessary shared libraries (e.g. inference_engine).
+        let build_path = cmake(out.to_str().unwrap())
+            .build_target("inference_engine_c_api")
+            .build();
+
+        // Unfortunately, `inference_engine_c_api` will not build the OpenVINO plugins used for
+        // the actual computation. Here we re-run CMake for each plugin the user specifies using
+        // Cargo features (see `Cargo.toml`).
+        for plugin in get_plugin_target_from_features() {
+            cmake(out.to_str().unwrap()).build_target(plugin).build();
+        }
+
+        // Set up the link search path to the manually-built library. It would be preferable to use
+        // pkg-config here to retrieve the libraries if they were installed system-wide (see initial
+        // commit), but OpenVINO relies on a `plugin.xml` file for finding target-specific libraries
+        // and it is unclear how pkg-config where this would be in the system-install scenario.
+        build_path.join("bin/intel64/Debug/lib")
+        // TODO we can document the assumption that OpenVINO will likely compile best on x86-64 but
+        // we must figure out what path the binaries have been created in: Debug/Release/RelWithDebInfo/MinSizeRel.
+        // The cmake crate is automatically inferring this: https://docs.rs/cmake/0.1.44/cmake/struct.Config.html#method.profile
+    };
     println!("cargo:rustc-link-search=native={}", library_path.display());
-    let third_party_path = build_path.join("lib64");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        third_party_path.display()
-    );
 
     // Dynamically link the OpenVINO libraries.
     let libraries = vec![
@@ -76,6 +104,18 @@ fn file<P: AsRef<Path>>(path: P) -> PathBuf {
     canonicalized
 }
 
+/// Canonicalize a path as well as verify that it exists.
+fn dir<P: AsRef<Path>>(path: P) -> PathBuf {
+    let canonicalized = path
+        .as_ref()
+        .canonicalize()
+        .expect("to be able to canonicalize the path");
+    if !canonicalized.exists() || !canonicalized.is_dir() {
+        panic!("Unable to find directory: {}", canonicalized.display())
+    }
+    canonicalized
+}
+
 /// Helper for recursively visiting the files in this directory; see https://doc.rust-lang.org/std/fs/fn.read_dir.html.
 fn visit_dirs(dir: &Path, cb: &dyn Fn(PathBuf)) -> std::io::Result<()> {
     if dir.is_dir() {
@@ -90,4 +130,35 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(PathBuf)) -> std::io::Result<()> {
         }
     }
     Ok(())
+}
+
+/// Determine CMake targets for the various OpenVINO plugins. The plugin mapping is available in
+/// OpenVINO's `plugins.xml` file and, usign that, this function wires up the exposed Cargo
+/// features of openvino-sys to the correct CMake target.
+fn get_plugin_target_from_features() -> Vec<&'static str> {
+    let mut plugins = vec![];
+    if cfg!(feature = "all") {
+        plugins.push("ie_plugins")
+    } else {
+        if cfg!(feature = "cpu") {
+            plugins.push("MKLDNNPlugin")
+        }
+        if cfg!(feature = "gpu") {
+            plugins.push("clDNNPlugin")
+        }
+        if cfg!(feature = "gna") {
+            plugins.push("GNAPlugin")
+        }
+        if cfg!(feature = "hetero") {
+            plugins.push("HeteroPlugin")
+        }
+        if cfg!(feature = "multi") {
+            plugins.push("MultiDevicePlugin")
+        }
+        if cfg!(feature = "myriad") {
+            plugins.push("myriadPlugin")
+        }
+    }
+    assert!(!plugins.is_empty());
+    plugins
 }
