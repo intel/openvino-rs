9e7e0c06b49fc47ec73190a82fc4a43e3ed32328
Andrew Brown <andrew.brown@intel.com>
2020-09-04 14:07:21 -0700

    Add CI build tasks
    This change instructs GitHub to build the libraries both from source and from pre-installed OpenVINO libraries (inside a Docker image)

--- crates/openvino-sys/build.rs
+++ crates/openvino-sys/build.rs
@@ -12,7 +12,7 @@ fn main() {
 
     // Generate bindings from C header.
     let openvino_c_api_header =
-        file("../upstream/inference-engine/ie_bridges/c/include/c_api/ie_c_api.h");
+        file("upstream/inference-engine/ie_bridges/c/include/c_api/ie_c_api.h");
     let bindings = bindgen::Builder::default()
         .header(openvino_c_api_header.to_string_lossy())
         // While understanding the warnings in https://docs.rs/bindgen/0.36.0/bindgen/struct.Builder.html#method.rustified_enum
@@ -30,17 +30,24 @@ fn main() {
         .write_to_file(out.join("bindings.rs"))
         .expect("failed to write bindings.rs");
 
-    // Build OpenVINO using CMake.
-    let library_path = if let Some(path) = std::env::var_os("OPENVINO_LIB_DIR") {
-        let library_path = dir(path);
-        assert!(
-            library_path.join("plugins.xml").is_file(),
-            "The OPENVINO_LIB_DIR should contain a plugins.xml file"
-        );
-        library_path
+    // Find OpenVINO libraries, either pre-installed or by building from source.
+    let library_paths = if let Some(path) = std::env::var_os("OPENVINO_INSTALL_DIR") {
+        // Given a path to an OpenVINO installation (e.g. https://docs.openvinotoolkit.org/latest/openvino_docs_install_guides_installing_openvino_apt.html),
+        // find the paths to the necessary libraries and add them to the library search path.
+        // It would be preferable to use pkg-config here to retrieve the libraries when they are
+        // installed system-wide but there are issues:
+        //  - OpenVINO does not install itself a system library, e.g., through ldconfig.
+        //  - OpenVINO relies on a `plugins.xml` file for finding target-specific libraries
+        //    and it is unclear how we would discover this in a system-install scenario.
+        let deployment_tools = dir(path).join("deployment_tools");
+        let openvino_libraries = deployment_tools.join("inference_engine/lib/intel64");
+        let tbb_libraries = deployment_tools.join("inference_engine/external/tbb/lib");
+        let ngraph_libraries = deployment_tools.join("ngraph/lib");
+        vec![openvino_libraries, tbb_libraries, ngraph_libraries]
     } else {
+        // Build OpenVINO with CMake.
         fn cmake(out_dir: &str) -> cmake::Config {
-            let mut config = cmake::Config::new("../upstream");
+            let mut config = cmake::Config::new("upstream");
             config
                 .very_verbose(true)
                 .define("NGRAPH_ONNX_IMPORT_ENABLE", "ON")
@@ -65,31 +72,96 @@ fn main() {
             cmake(out.to_str().unwrap()).build_target(plugin).build();
         }
 
-        // Set up the link search path to the manually-built library. It would be preferable to use
-        // pkg-config here to retrieve the libraries if they were installed system-wide (see initial
-        // commit), but OpenVINO relies on a `plugin.xml` file for finding target-specific libraries
-        // and it is unclear how pkg-config where this would be in the system-install scenario.
-        build_path.join("bin/intel64/Debug/lib")
-        // TODO we can document the assumption that OpenVINO will likely compile best on x86-64 but
-        // we must figure out what path the binaries have been created in: Debug/Release/RelWithDebInfo/MinSizeRel.
-        // The cmake crate is automatically inferring this: https://docs.rs/cmake/0.1.44/cmake/struct.Config.html#method.profile
-    };
-    println!("cargo:rustc-link-search=native={}", library_path.display());
+        // Collect the locations of the libraries. Note that ngraph should also be found with the
+        // built OpenVINO libraries.
+        let openvino_libraries =
+            find_and_append_cmake_build_type(build_path.join("bin/intel64")).join("lib");
 
-    // Dynamically link the OpenVINO libraries.
+        // Copy the TBB libraries into the OpenVINO library directory. Since ngraph already exists
+        // here and because the TBB directory is weirdly downloaded in-tree rather than under target
+        // (meaning that the TBB path would be stripped from LD_LIBRARY_PATH, see
+        // https://doc.rust-lang.org/cargo/reference/environment-variables.html#dynamic-library-paths),
+        // copying the files over makes some sense. Also, I have noticed compatibility issues with
+        // pre-installed libtbb (on some systems, the nodes_count symbol is not present in the
+        // system-provided libtbb) so it may be important to include OpenVINO's version of libtbb
+        // here.
+        let tbb_libraries = dir("upstream/inference-engine/temp/tbb/lib");
+        visit_dirs(&tbb_libraries, &|from: PathBuf| {
+            let to = openvino_libraries.join(from.file_name().unwrap());
+            println!("Copying {} to {}", from.display(), to.display());
+            std::fs::copy(from, to);
+        });
+
+        vec![openvino_libraries]
+    };
+
+    // Check that the plugins.xml file exists and create an environment variable recording this
+    // location.
+    let openvino_plugins = library_paths[0].join("plugins.xml");
+    assert!(
+        openvino_plugins.is_file(),
+        "Unable to find OpenVINO plugins.xml file at: {}",
+        openvino_plugins.display()
+    );
+    println!(
+        "cargo:rustc-env=OPENVINO_LIB_DIR={}",
+        library_paths[0].display()
+    );
+
+    // Configure the build-time library search paths.
+    for library in &library_paths {
+        add_buildtime_library_path(library);
+    }
+
+    // Dynamically link the necessary OpenVINO libraries.
     let libraries = vec![
         "inference_engine",
         "inference_engine_legacy",
         "inference_engine_transformations",
-        "ngraph",
         "inference_engine_c_api",
+        "ngraph",
+        "tbb",
     ];
     for library in &libraries {
         println!("cargo:rustc-link-lib=dylib={}", library);
     }
+}
 
-    // Output the location of the libraries to the environment.
-    println!("cargo:rustc-env=LIBRARY_PATH={}", library_path.display());
+/// Ensure a path is valid and add it to the build-time library search path.
+fn add_buildtime_library_path<P: AsRef<Path>>(path: P) {
+    let path = path.as_ref();
+    assert!(
+        path.is_dir(),
+        "Invalid library search path: {}",
+        path.display()
+    );
+    println!("cargo:rustc-link-search=native={}", path.display());
+}
+
+/// Add a path (or concatenated paths with ':') to rustc's runtime library search path. Note that
+/// this will *NOT* set up the library paths for executables (e.g. cargo test, cargo run).
+#[allow(dead_code)]
+fn add_runtime_library_paths(paths: &[PathBuf]) {
+    let var_name = if cfg!(target_os = "linux") {
+        "LD_LIBRARY_PATH"
+    } else if cfg!(target_os = "macos") {
+        "DYLD_FALLBACK_LIBRARY_PATH"
+    } else if cfg!(target_os = "windows") {
+        "PATH"
+    } else {
+        panic!("No way to know how to add to the runtime library search path")
+    };
+
+    let concatenated_library_paths = paths
+        .iter()
+        .map(|l| l.to_str().unwrap())
+        .collect::<Vec<_>>()
+        .join(":");
+
+    println!(
+        "cargo:rustc-env={}={}",
+        var_name, concatenated_library_paths
+    );
 }
 
 /// Canonicalize a path as well as verify that it exists.
@@ -125,7 +197,7 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(PathBuf)) -> std::io::Result<()> {
             if path.is_dir() {
                 visit_dirs(&path, cb)?;
             } else {
-                cb(entry.path());
+                cb(path);
             }
         }
     }
@@ -161,4 +233,29 @@ fn get_plugin_target_from_features() -> Vec<&'static str> {
     }
     assert!(!plugins.is_empty());
     plugins
+}
+
+/// According to https://docs.rs/cmake/0.1.44/cmake/struct.Config.html#method.profile, the cmake
+/// crate will tries to infer the appropriate CMAKE_BUILD_TYPE from a combination of Rust opt-level
+/// and debug. To avoid duplicating https://docs.rs/cmake/0.1.44/src/cmake/lib.rs.html#553-559, this
+/// helper searches for build type directories and appends it to the path if a result is found; this
+/// will panic otherwise.
+fn find_and_append_cmake_build_type(build_path: PathBuf) -> PathBuf {
+    let types = ["Debug", "Release", "RelWithDebInfo", "MinSizeRel"];
+    let found: Vec<_> = types
+        .iter()
+        .filter(|&&t| build_path.join(t).is_dir())
+        .collect();
+    match found.len() {
+        0 => panic!(
+            "No CMake build directory found in {}; expected one of {:?}",
+            build_path.display(),
+            types
+        ),
+        1 => build_path.join(found[0]),
+        _ => panic!(
+            "Too many CMake build directories found in {}",
+            build_path.display()
+        ),
+    }
 }
--- crates/openvino-sys/src/lib.rs
+++ crates/openvino-sys/src/lib.rs
@@ -9,11 +9,11 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 /// built. __Warning__: do not use this on systems other than the system that built `openvino-sys`.
 ///
 /// Its presence here is necessary because OpenVINO loads target-specific libraries on demand for
-/// performing inference. To do so, it relies on a `plugin.xml` file that maps targets (e.g. CPU)
+/// performing inference. To do so, it relies on a `plugins.xml` file that maps targets (e.g. CPU)
 /// to the target-specific implementation library. At runtime, it inspects this file and loads the
-/// libraries to satisfy the user's specified targets. By default, the `plugin.xml` file and these
+/// libraries to satisfy the user's specified targets. By default, the `plugins.xml` file and these
 /// libraries will be available at this path.
-pub const LIBRARY_PATH: &'static str = env!("LIBRARY_PATH");
+pub const LIBRARY_PATH: &'static str = env!("OPENVINO_LIB_DIR");
 
 #[cfg(test)]
 mod tests {
@@ -23,9 +23,6 @@ mod tests {
     #[test]
     fn check_version() {
         let version = unsafe { CStr::from_ptr(ie_c_api_version().api_version) };
-        assert_eq!(
-            version.to_string_lossy(),
-            "2.1.custom_master_a0581d3d8f004c27d7ce118a91be18b7c8cafb87"
-        );
+        assert!(version.to_string_lossy().starts_with("2.1"));
     }
 }
