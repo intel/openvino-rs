2139f84ce873c6a2969d531935ea0762d267b214
Bradley Odell <btodell@hotmail.com>
2024-04-15 19:13:42 -0700

    Working on infer request callback support...

--- crates/openvino/src/request.rs
+++ crates/openvino/src/request.rs
@@ -7,9 +7,7 @@ use openvino_sys::{
     ov_infer_request_wait_for,
 };
 use std::ffi::c_void;
-use std::future::Future;
-use std::pin::Pin;
-use std::task::{Context, Poll};
+use std::marker::PhantomData;
 use std::time::Duration;
 
 /// See [`InferRequest`](https://docs.openvino.ai/2024/api/c_cpp_api/group__ov__infer__request__c__api.html).
@@ -49,21 +47,6 @@ impl InferRequest {
         try_unsafe!(ov_infer_request_infer(self.instance))
     }
 
-    /// Starts executing the inference request and asynchronously wait for the result.
-    pub fn infer_async(&mut self) -> impl Future<Output = Result<()>> {
-        let result = async {
-            // Set callback
-            let callback = InferRequestClosure::set_callback(self, || {
-                // TODO notify future complete (how?)
-            })?;
-            self.start_async()
-            // TODO what if an error happens here and the callback goes out of scope?
-            // If the callback goes out of scope, the request needs to be cancelled or
-            // the callback needs to be set to a callback with a static lifetime
-        };
-        InferAsync {}
-    }
-
     /// Starts executing the inference request asynchronously (non-blocking).
     pub fn start_async(&mut self) -> Result<()> {
         try_unsafe!(ov_infer_request_start_async(self.instance))
@@ -83,18 +66,31 @@ impl InferRequest {
         try_unsafe!(ov_infer_request_wait_for(self.instance, timeout_ms))
     }
 
-    // /// Sets a callback function that is called on success or failure of an asynchronous request.
-    // pub fn set_callback<'a, F>(&'a mut self, callback: F) -> Result<()>
-    // where
-    //     F: FnMut() + 'a,
-    // {
-    //     // TODO how to require closure F to live as long as 'self'?
-    //     let callback = InferRequestCallback::new(callback);
-    //     try_unsafe!(ov_infer_request_set_callback(
-    //         self.instance,
-    //         std::ptr::addr_of!(callback.instance)
-    //     ))
-    // }
+    /// Sets a callback function that is called on success or failure of an asynchronous request.
+    ///
+    /// If the returned handle is dropped, the callback will be automatically cleared
+    /// from the infer request. Therefore, the handle MUST be kept in memory as long as it's desired
+    /// to receive callbacks.
+    pub fn set_callback<'a, F>(
+        &mut self,
+        callback: &'a mut F,
+    ) -> Result<InferRequestCallbackHandle<'a, F>>
+    where
+        F: FnMut(),
+    {
+        let ov_callback = ov_callback_t {
+            callback_func: Some(trampoline::<F>),
+            args: callback as *mut F as *mut c_void,
+        };
+        try_unsafe!(ov_infer_request_set_callback(
+            self.instance,
+            std::ptr::addr_of!(ov_callback)
+        ))?;
+        Ok(InferRequestCallbackHandle {
+            boo: PhantomData,
+            request: self.instance,
+        })
+    }
 
     /// Cancels inference request.
     pub fn cancel(&mut self) -> Result<()> {
@@ -103,30 +99,29 @@ impl InferRequest {
 }
 
 /// Type-safe closure for infer request async callbacks.
-struct InferRequestClosure<F>
+pub struct InferRequestCallbackHandle<'a, F>
 where
-    F: FnMut(),
+    F: FnMut() + 'a,
 {
-    func: F,
+    boo: PhantomData<&'a F>,
+    request: *mut ov_infer_request_t,
 }
 
-impl<F> InferRequestClosure<F>
+impl<F> Drop for InferRequestCallbackHandle<'_, F>
 where
     F: FnMut(),
 {
-    pub fn set_callback(request: &mut InferRequest, mut func: F) -> Result<Self> {
-        let callback = ov_callback_t {
-            callback_func: Some(trampoline::<F>),
-            args: &mut func as *mut _ as *mut c_void,
+    fn drop(&mut self) {
+        // If callback handle is dropped, automatically clear it from infer request
+        let ov_callback = ov_callback_t {
+            callback_func: Some(noop),
+            args: std::ptr::null_mut(),
         };
-        try_unsafe!(ov_infer_request_set_callback(
-            request.instance,
-            std::ptr::addr_of!(callback)
-        ))?;
-        Ok(Self { func })
+        unsafe { ov_infer_request_set_callback(self.request, std::ptr::addr_of!(ov_callback)) };
     }
 }
 
+/// A trampoline C/FFI function to launch into the Rust closure
 unsafe extern "C" fn trampoline<F>(user_data: *mut c_void)
 where
     F: FnMut(),
@@ -135,14 +130,5 @@ where
     user_data();
 }
 
-struct InferAsync {
-    // TODO
-}
-
-impl Future for InferAsync {
-    type Output = Result<()>;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        todo!()
-    }
-}
+/// An empty C/FFI function used to clear the callback for an infer request
+unsafe extern "C" fn noop(_: *mut c_void) {}
