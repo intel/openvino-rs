0b0241b8aa41e92622fa84b62965b6f82380626d
Andrew Brown <andrew.brown@intel.com>
2024-09-20 10:56:56 -0700

    Add `xtask` to update upstream submodule
    Every so often the upstream OpenVINO repository releases a new version.
    This new task, `cargo xtask update <tag>`, will run some Git commands to
    update Git submodule tracked by this repository. Since each release may
    slightly tweak the header files, this new task is expected to be used
    immediately prior to `cargo xtask codegen; cargo test`.

---
+++ crates/xtask/src/update.rs
@@ -0,0 +1,23 @@
+use crate::util::{exec, path_to_crates};
+use anyhow::Result;
+use clap::Args;
+use std::process::Command;
+
+#[derive(Debug, Args)]
+pub struct UpdateCommand {
+    /// The upstream tag to update to; see https://github.com/openvinotoolkit/openvino/releases.
+    #[arg(name = "TAG")]
+    tag: String,
+}
+
+impl UpdateCommand {
+    /// Retrieve the requested tag and checkout the upstream submodule using `git`.
+    pub fn execute(&self) -> Result<()> {
+        let submodule = path_to_crates()?.join("openvino-sys/upstream");
+        let submodule = submodule.to_string_lossy();
+        exec(Command::new("git").args(["-C", &submodule, "fetch", "origin", "tag", &self.tag]))?;
+        exec(Command::new("git").args(["-C", &submodule, "checkout", &self.tag]))?;
+        println!("> to use the updated headers, run `cargo xtask codegen`");
+        Ok(())
+    }
+}
