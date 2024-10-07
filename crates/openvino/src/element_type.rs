4e85321d362499c16985d3162c2e09040e2d03e9
Andrew Brown <andrew.brown@intel.com>
2024-10-07 10:51:38 -0700

    doc: tweak order of external links for consistency

--- crates/openvino/src/element_type.rs
+++ crates/openvino/src/element_type.rs
@@ -12,7 +12,8 @@ use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt;
 
-/// `ElementType` represents the type of elements that a tensor can hold. See [`ElementType`](https://docs.openvino.ai/2024/api/c_cpp_api/group__ov__base__c__api.html#_CPPv417ov_element_type_e).
+/// See
+/// [`ElementType`](https://docs.openvino.ai/2024/api/c_cpp_api/group__ov__base__c__api.html#_CPPv417ov_element_type_e).
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 #[repr(u32)]
 pub enum ElementType {
